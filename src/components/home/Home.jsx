9fa2aac4a77ea690fc1fcbb1deb41f57ecb36017
baudoin <baudoinvicbolingo@gmail.com>
2024-02-23 23:15:34 +0200

    adding comments

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -12,9 +12,9 @@ const Home = () => {
       <Hero />
       <AboutCard />
       <HAbout />
-      <Testimonal />
+      {/* <Testimonal /> */}
       <Hblog />
-      <Hprice />
+      {/* <Hprice /> */}
     </>
   )
 }
