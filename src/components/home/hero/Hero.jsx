59441f4461df80e1c503ce6d9835ac2236210a78
baudoin <baudoinvicbolingo@gmail.com>
2024-02-23 23:00:14 +0200

    modifying text

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
       <section className='hero'>
         <div className='container'>
           <div className='row'>
-            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <Heading subtitle='WELCOME TO LEPIEUX INSTITUTE' title='Best High School Education Expertise' />
+            <p>We're happy to connect with you in whatever way you're most comfortable</p>
             <div className='button'>
               <button className='primary-btn'>
                 GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
