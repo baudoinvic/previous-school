b2735c75f932ee1a15412bd84fee35cb50b5583a
baudoin <baudoinvicbolingo@gmail.com>
2024-02-24 13:32:52 +0200

    changing footer some texts

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -48,27 +48,27 @@ const Footer = () => {
               <li>Feedbacks</li>
             </ul>
           </div>
-          <div className='box'>
-            <h3>Recent Post</h3>
-            {blog.slice(0, 3).map((val) => (
-              <div className='items flexSB'>
-                <div className='img'>
-                  <img src={val.cover} alt='' />
-                </div>
-                <div className='text'>
-                  <span>
-                    <i className='fa fa-calendar-alt'></i>
-                    <label htmlFor=''>{val.date}</label>
-                  </span>
-                  <span>
-                    <i className='fa fa-user'></i>
-                    <label htmlFor=''>{val.type}</label>
-                  </span>
-                  <h4>{val.title.slice(0, 40)}...</h4>
-                </div>
-              </div>
-            ))}
+        
+             
+          <div className='box link'>
+            <h3>Address</h3>
+            <ul>
+              <li>Congo Drc</li>
+              <li>Kalemie 240 street</li>
+              <li>Terms & Conditions</li>
+              <li>Tanganyika Province</li>
+              <li>Kalemie city</li>
+            </ul>
           </div>
+
+
+
+
+
+
+
+
+
           <div className='box last'>
             <h3>Have a Questions?</h3>
             <ul>
