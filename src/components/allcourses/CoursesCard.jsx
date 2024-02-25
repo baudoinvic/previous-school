9459fbd574ecaf464143a732e9d8b15a19e20065
baudoin <baudoinvicbolingo@gmail.com>
2024-02-25 18:35:21 +0200

    changing text

--- src/components/allcourses/CoursesCard.jsx
+++ src/components/allcourses/CoursesCard.jsx
@@ -6,57 +6,59 @@ import Heading from "../common/heading/Heading"
 const CoursesCard = () => {
   return (
     <>
-      <section className='coursesCard'>
-      <div className="container">
-    <Heading subtitle='our courses' title='explore our popular online courses' />
-    
- </div>
-        <div className='container grid2'>
+      <section className="coursesCard">
+        <div className="container">
+          <Heading
+            subtitle="our courses"
+            title="Explore Our Range of Courses"
+          />
+        </div>
+        <div className="container grid2">
           {coursesCard.map((val) => (
-            <div className='items'>
-              <div className='content flex'>
-                <div className='left'>
-                  <div className='img'>
-                    <img src={val.cover} alt='' />
+            <div className="items">
+              <div className="content flex">
+                <div className="left">
+                  <div className="img">
+                    <img src={val.cover} alt="" />
                   </div>
                 </div>
-                <div className='text'>
+                <div className="text">
                   <h1>{val.coursesName}</h1>
-                  <p>{val.desc}</p> 
-                  <div className='rate'>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
+                  <p>{val.desc}</p>
+                  <div className="rate">
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
                     {/* <label htmlFor=''>(5.0)</label> */}
                   </div>
-                  <div className='details'>
-               {val.courTeacher && val.courTeacher.map((details) => (
-              <React.Fragment key={details.name}>
-             <div className='box'>
-           <div className='dimg'>
-          <img src={details.dcover} alt='' />
-             </div>
-            <div className='para'>
-             <h4>{details.name}</h4>
-           </div>
-           </div>
-         <span>{details.totalTime}</span>
-    </React.Fragment>
-  ))}
-</div>
-
+                  <div className="details">
+                    {val.courTeacher &&
+                      val.courTeacher.map((details) => (
+                        <React.Fragment key={details.name}>
+                          <div className="box">
+                            <div className="dimg">
+                              <img src={details.dcover} alt="" />
+                            </div>
+                            <div className="para">
+                              <h4>{details.name}</h4>
+                            </div>
+                          </div>
+                          <span>{details.totalTime}</span>
+                        </React.Fragment>
+                      ))}
+                  </div>
                 </div>
               </div>
-             
+
               {/* <button className='outline-btn'>ENROLL NOW </button> */}
             </div>
           ))}
         </div>
       </section>
     </>
-  )
+  );
 }
 
 export default CoursesCard
